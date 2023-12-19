f3c593251bf5ab1da9d456c859cfca0fcb5d2bb4
ninglang <yzmninglang@gmail.com>
2023-12-20 00:08:23 +0800

    Init Project

--- autocommit.bat
+++ autocommit.bat
@@ -1,5 +1,5 @@
 set data=%date:~3,10%
-set currentDate=%data:~0,4%-%data:~5,2%-%data:~8,2%-%time:~1,2%-%time:~3,2%-%time:~6,2%.zip
+set currentDate=%data:~0,4%-%data:~5,2%-%data:~8,2%-%time:~1,1%-%time:~3,2%-%time:~6,2%.zip
 
 git init
 git add .
